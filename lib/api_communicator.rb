98c303622839a07cf011c21f0c26407ecda16d50
github username <github email address>
2019-02-19 18:11:11 +0000

    Louie and Laura extra method

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,21 +3,19 @@ require 'pry'
 require 'rest-client'
 require 'json'
 
+
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get('http://www.swapi.co/api/people/')   #make the web request
   response_hash = JSON.parse(response_string)
   results = response_hash["results"]
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  films = nil
+  # iterate over the response hash to find the collection of `films` for the given `character`
 
+  films = nil
   results.each do |character|
     if character_name == character["name"]
       films = character["films"]
     end
-
   end
 
   if films
@@ -31,8 +29,8 @@ def get_character_movies_from_api(character_name)
   end
 
   film_data
-
 end
+
 def print_movies(films)
   if films
     films.each_with_index do |film, index|
@@ -48,3 +46,43 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+#.............................. new method
+
+def get_movie_data
+  response_string = RestClient.get('http://www.swapi.co/api/films/')   #make the web request
+  movie_hash = JSON.parse(response_string)
+  results_movies = movie_hash["results"]
+end
+
+
+def get_character_names(movie_name)
+ results_movies = get_movie_data
+  results_movies.each do |movie_info|
+    if movie_info["title"] == movie_name
+    return movie_info["characters"]
+   end
+ end
+end
+
+def display_data(movie_name)
+  character_data = get_character_names(movie_name)
+
+  character_data.each_with_index do |character, index|
+    response_string = RestClient.get(character)
+      character_hash = JSON.parse(response_string)
+    puts "#{index +1}. #{character_hash["name"]}"
+  end
+end
+
+display_data("A New Hope")
+
+
+
+# 1. get data from the IP :)
+# 2. parse data into json :)
+# 3. save the film data :)
+
+# 4. get character names from movie hash using the movie name :)
+
+# 5. puts out the character data into a list
+# 6. catering for inconsistencies in data inputs vs API data
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,6 +3,6 @@ def welcome
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name"
   gets.chomp
 end
